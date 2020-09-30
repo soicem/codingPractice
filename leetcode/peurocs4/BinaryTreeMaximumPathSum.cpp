f3833f897e68970ed7ec12c11b4ad9b6441bdf12
CSKim <peurocs4@gmail.com>
2020-09-30 17:26:06 +0900

    Add "Binary Tree Maximum Path Sum"

---
+++ leetcode/peurocs4/BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,42 @@
+#ifndef BinaryTreeMaximumPathSum_h__
+#define BinaryTreeMaximumPathSum_h__
+
+
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int maxSum = INT_MIN;
+
+  int maxPathSum(TreeNode* root) {
+    maxSum = INT_MIN;
+    traverse(root);
+    return maxSum;
+  }
+
+  int traverse(TreeNode* node) {
+    if (node == nullptr) { return 0; }
+
+    const int left = max(0, traverse(node->left));
+    const int right = max(0, traverse(node->right));
+    const int sum = node->val + left + right;
+    maxSum = max(maxSum, sum);
+
+    return node->val + max(left, right);
+  }
+};
+
+
+#endif // BinaryTreeMaximumPathSum_h__

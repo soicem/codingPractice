f7a4168f9160fbdc74cb75fd4f460a713b225813
CSKim <peurocs4@gmail.com>
2020-10-02 17:35:08 +0900

    Add "Flip Equivalent Binary Trees"

---
+++ leetcode/peurocs4/FlipEquivalentBinaryTrees.cpp
@@ -0,0 +1,34 @@
+#ifndef FlipEquivalentBinaryTrees_h__
+#define FlipEquivalentBinaryTrees_h__
+
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+    if (root1 == nullptr || root2 == nullptr) {
+      return root1 == root2;
+    }
+
+    if (root1->val != root2->val) { return false; }
+
+    bool isSame = flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
+    if (isSame) { return true; }
+
+    bool canFlip = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
+    return canFlip;
+  }
+};
+
+
+#endif // FlipEquivalentBinaryTrees_h__

4dcf39ffa456a162d54c4d604d35b77a5c70596c
CSKim <peurocs4@gmail.com>
2020-10-02 15:11:57 +0900

    Add "Diameter Of Binary Tree"

---
+++ leetcode/peurocs4/DiameterOfBinaryTree.cpp
@@ -0,0 +1,38 @@
+#ifndef DiameterOfBinaryTree_h__
+#define DiameterOfBinaryTree_h__
+
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int maxDiameter = 0;
+
+  int diameterOfBinaryTree(TreeNode* root) {
+    maxDiameter = 0;
+    traverse(root);
+    return maxDiameter;
+  }
+
+  int traverse(TreeNode* node) {
+    if (node == nullptr) { return 0; }
+
+    int left = traverse(node->left);
+    int right = traverse(node->right);
+
+    maxDiameter = max(maxDiameter, left + right);
+    return max(left, right) + 1;
+  }
+};
+
+
+#endif // DiameterOfBinaryTree_h__

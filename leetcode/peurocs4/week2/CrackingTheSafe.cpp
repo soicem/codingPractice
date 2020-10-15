ee1dec65edcd6ca8052fac320d89c256a580bdfc
CSKim <peurocs4@gmail.com>
2020-10-15 17:02:35 +0900

    add "Cracking the Safe"

---
+++ leetcode/peurocs4/week2/CrackingTheSafe.cpp
@@ -0,0 +1,46 @@
+#ifndef CrackingTheSafe_h__
+#define CrackingTheSafe_h__
+
+
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+  stringstream answer;
+  unordered_set<string> used;
+
+  string crackSafe(int n, int k) {
+    if (n == 1 && k == 1) { return "0"; }
+
+    stringstream ss;
+    for (int i = 0; i < n - 1; ++i) {
+      ss << '0';
+    }
+    string start = ss.str();
+
+    dfs(start, k);
+    answer << start;
+    return answer.str();
+  }
+
+  void dfs(string start, int k) {
+    for (int i = 0; i < k; ++i) {
+      string newString(start);
+      newString.push_back(i + '0');
+
+      if (used.count(newString) == 0) {
+        used.insert(newString);
+        dfs(newString.substr(1), k);
+        answer << i;
+      }
+    }
+  }
+};
+
+
+#endif // CrackingTheSafe_h__

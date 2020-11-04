a265f722118be90537cbcf99ee54e0c51b2f9f9d
CSKim <peurocs4@gmail.com>
2020-11-04 13:51:04 +0900

    improve "Median of Two Sorted Arrays"

--- leetcode/peurocs4/week4/MedianOfTwoSortedArrays.h
+++ leetcode/peurocs4/week4/MedianOfTwoSortedArrays.h
@@ -1,23 +1,39 @@
 #pragma once
-#include <algorithm>
 #include <vector>
 
 class Solution {
 public:
   double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
-    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-    std::sort(nums1.begin(), nums1.end());
-
-    if (nums1.empty()) { return 0.0; }
-
-    if (nums1.size() % 2 == 0) {
-      int mid1 = nums1.size() / 2;
-      int mid2 = 0.5 + nums1.size() / 2.0;
-      return nums1[mid1] + (nums1[mid2] - nums1[mid1]) / 2;
+    if (nums1.empty() && nums2.empty()) { return 0.0; }
+    if (nums1.empty()) {
+      auto mid1 = nums2.size() / 2.0 - 0.5;
+      auto mid2 = nums2.size() / 2.0;
+      return nums2[mid1] + (nums2[mid2] - nums2[mid1]) / 2.0;
     }
-    else {
-      int mid = nums1.size() / 2;
-      return nums1[mid];
+    if (nums2.empty()) {
+      auto mid1 = nums1.size() / 2.0 - 0.5;
+      auto mid2 = nums1.size() / 2.0;
+      return nums1[mid1] + (nums1[mid2] - nums1[mid1]) / 2.0;
     }
+
+    auto totalSize = nums1.size() + nums2.size();
+    auto count = 0;
+    auto i1 = 0;
+    auto i2 = 0;
+    auto val1 = 0;
+    auto val2 = 0;
+    while (count <= totalSize / 2) {
+      val1 = val2;
+      if (i1 < nums1.size() && (i2 >= nums2.size() || nums1[i1] < nums2[i2])) {
+        val2 = nums1[i1];
+        ++i1;
+      }
+      else {
+        val2 = nums2[i2];
+        ++i2;
+      }
+      ++count;
+    }
+    return totalSize % 2 ? val2 : val1 + (val2 - val1) / 2.0;
   }
 };
